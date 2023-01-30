c951a65744669140886ea67ef688cfd44b099658
chenjialing <595168663@qq.com>
2023-01-30 18:49:02 +0800

    修改----心跳联调增加过期时间

--- rundo-gb28181-server/src/main/java/com/runjian/mq/gatewayInfo/GatewayHeartBeatSchedule.java
+++ rundo-gb28181-server/src/main/java/com/runjian/mq/gatewayInfo/GatewayHeartBeatSchedule.java
@@ -6,10 +6,13 @@ import com.runjian.common.constant.MarkConstant;
 import com.runjian.common.mq.RabbitMqSender;
 import com.runjian.common.mq.domain.GatewayMqDto;
 import com.runjian.common.utils.ConstantUtils;
+import com.runjian.common.utils.DateUtils;
+import com.runjian.common.utils.UuidUtil;
 import com.runjian.service.IRedisCatchStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -18,6 +21,7 @@ import java.time.LocalDateTime;
  * 定时发送心跳
  * @author chenjialing
  */
+@Component
 public class GatewayHeartBeatSchedule {
 
     @Value("${gateway-info.serialNum}")
@@ -26,6 +30,9 @@ public class GatewayHeartBeatSchedule {
     private IRedisCatchStorageService iRedisCatchStorageService;
     @Autowired
     private RabbitMqSender rabbitMqSender;
+
+    @Value("${gateway-info.expire}")
+    private int expire;
 
     @Scheduled(cron="0 0/1 * * * ?")   //每1分钟执行一次
     public void sendMsg(){
@@ -37,7 +44,8 @@ public class GatewayHeartBeatSchedule {
         String sn = iRedisCatchStorageService.getSn(GatewayCacheConstants.GATEWAY_INFO_SN_INCR);
 
         gatewayMqDto.setMsgId(GatewayCacheConstants.GATEWAY_INFO_SN_prefix+sn);
-//消息组装
-        rabbitMqSender.sendMsg(MarkConstant.SIGIN_SG,  ""+ Instant.now().toEpochMilli() + ConstantUtils.RANDOM_UTIL.nextInt(100), gatewayMqDto, true);
+        gatewayMqDto.setData(DateUtils.getExpireTimestamp(expire));
+        //消息组装
+        rabbitMqSender.sendMsg(MarkConstant.SIGIN_SG, UuidUtil.toUuid(), gatewayMqDto, true);
     }
 }
