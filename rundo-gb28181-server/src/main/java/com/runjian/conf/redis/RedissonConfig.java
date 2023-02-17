327e29ebc9631c23465abf57d221cdd1ca78eb76
chenjialing <595168663@qq.com>
2023-02-17 14:59:40 +0800

    修改--redisson配置修改

--- rundo-gb28181-server/src/main/java/com/runjian/conf/redis/RedissonConfig.java
+++ rundo-gb28181-server/src/main/java/com/runjian/conf/redis/RedissonConfig.java
@@ -3,9 +3,11 @@ package com.runjian.conf.redis;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ObjectUtils;
 
 import java.io.IOException;
 
@@ -14,10 +16,28 @@ import java.io.IOException;
  */
 @Configuration
 public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
+    @Value("${spring.redis.database}")
+    private Integer redisDatabase;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
     @Bean(destroyMethod="shutdown")
     public RedissonClient redisson() throws IOException {
-        RedissonClient redisson = Redisson.create(
-                Config.fromYAML(new ClassPathResource("redisson-single.yml").getInputStream()));
-        return redisson;
+        Config config = Config.fromYAML(new ClassPathResource("redisson-single.yml").getInputStream());
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setDatabase(redisDatabase);
+        if(!ObjectUtils.isEmpty(password)){
+            config.useSingleServer().setPassword(password);
+        }
+        return Redisson.create(config);
     }
 }
