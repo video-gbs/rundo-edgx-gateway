f7796bd6bb753bd52b03acca3d6e4a90b56117e2
chenjialing <595168663@qq.com>
2023-04-26 18:50:36 +0800

    开发--关闭ssrc

--- rundo-media-dispatcher/src/main/java/com/runjian/media/dispatcher/zlm/service/impl/MediaServerServiceImpl.java
+++ rundo-media-dispatcher/src/main/java/com/runjian/media/dispatcher/zlm/service/impl/MediaServerServiceImpl.java
@@ -154,7 +154,7 @@ public class MediaServerServiceImpl implements ImediaServerService {
 
         int rtpServerPort;
         if (mediaServerItem.isRtpEnable()) {
-            rtpServerPort = zlmrtpServerFactory.createRTPServer(mediaServerItem, streamId, ssrcCheck?Integer.parseInt(ssrc):0, port);
+            rtpServerPort = zlmrtpServerFactory.createRTPServer(mediaServerItem, streamId, 0, port);
         } else {
             // todo 暂时不考虑单端口服用的情况
             rtpServerPort = mediaServerItem.getRtpProxyPort();
