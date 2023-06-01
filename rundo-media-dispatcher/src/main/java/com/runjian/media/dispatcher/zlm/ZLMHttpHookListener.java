f57364bb029564a1630f8b3a872f1abc70e0c054
chenjialing <595168663@qq.com>
2023-06-01 11:10:23 +0800

    修改--无人观看异常

--- rundo-media-dispatcher/src/main/java/com/runjian/media/dispatcher/zlm/ZLMHttpHookListener.java
+++ rundo-media-dispatcher/src/main/java/com/runjian/media/dispatcher/zlm/ZLMHttpHookListener.java
@@ -310,7 +310,7 @@ public class ZLMHttpHookListener {
 		ret.put("code", 0);
 		//是否关闭推拉流 交给上层业务判断
 		Boolean aBoolean = mediaPlayService.onStreamNoneReader(app, streamId);
-		ret.put("close", aBoolean);
+		ret.put("close", false);
 
 		return ret;
 	}
