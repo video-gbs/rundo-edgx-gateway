2236eee83dee9f41889aed8dc3fdc072b0b41797
chenjialing <595168663@qq.com>
2023-10-24 09:45:06 +0800

    zlm调度--增加webrtc发送日志

--- rundo-media-dispatcher/src/main/java/com/runjian/media/dispatcher/mq/MqMsgDealService/impl/StreamWebrtcTalkMsgServiceImpl.java
+++ rundo-media-dispatcher/src/main/java/com/runjian/media/dispatcher/mq/MqMsgDealService/impl/StreamWebrtcTalkMsgServiceImpl.java
@@ -3,7 +3,10 @@ package com.runjian.media.dispatcher.mq.MqMsgDealService.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.runjian.common.commonDto.Gb28181Media.req.MediaPlayReq;
 import com.runjian.common.commonDto.Gb28181Media.req.WebRTCTalkReq;
+import com.runjian.common.commonDto.StreamInfo;
+import com.runjian.common.config.exception.BusinessErrorEnums;
 import com.runjian.common.constant.GatewayCacheConstants;
+import com.runjian.common.constant.LogTemplate;
 import com.runjian.common.constant.StreamBusinessMsgType;
 import com.runjian.common.mq.RabbitMqSender;
 import com.runjian.common.mq.domain.CommonMqDto;
@@ -13,11 +16,13 @@ import com.runjian.media.dispatcher.mq.MqMsgDealService.IMqMsgDealServer;
 import com.runjian.media.dispatcher.mq.MqMsgDealService.IMsgProcessorService;
 import com.runjian.media.dispatcher.service.IMediaPlayService;
 import com.runjian.media.dispatcher.service.IRedisCatchStorageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class StreamWebrtcTalkMsgServiceImpl implements InitializingBean, IMsgProcessorService {
 
     @Autowired
@@ -51,12 +56,24 @@ public class StreamWebrtcTalkMsgServiceImpl implements InitializingBean, IMsgPro
         playReq.setGatewayMqRouteKey(dataMapJson.getString("gatewayMq"));
         playReq.setMsgId(commonMqDto.getMsgId());
         playReq.setDispatchUrl(dataMapJson.getString("mediaUrl"));
-        String webRtcTalkUrl = iMediaPlayService.webRtcTalk(playReq);
-//进行消息返回
+        String webRtcTalkUrl = "";
         CommonMqDto businessMqInfo = redisCatchStorageService.getMqInfo(StreamBusinessMsgType.STREAM_WEBRTC_TALK.getTypeName(), GatewayCacheConstants.DISPATCHER_BUSINESS_SN_INCR, GatewayCacheConstants.GATEWAY_BUSINESS_SN_prefix,commonMqDto.getMsgId());
+        try {
+            webRtcTalkUrl = iMediaPlayService.webRtcTalk(playReq);
+        }catch (Exception e){
+            businessMqInfo.setCode(BusinessErrorEnums.UNKNOWN_ERROR.getErrCode());
+            businessMqInfo.setMsg(e.getMessage());
+
+            log.error(LogTemplate.PROCESS_LOG_MSG_TEMPLATE, "zlm的webrtc注册失败", "失败信息", e);
+        }
+        //进行消息返回
+        StreamInfo streamInfo = new StreamInfo();
+        streamInfo.setRtc(webRtcTalkUrl);
+
         String mqGetQueue = dispatcherSignInConf.getMqSetQueue();
         //通知调度中心成功
-        businessMqInfo.setData(webRtcTalkUrl);
+        businessMqInfo.setData(streamInfo);
+        log.info(LogTemplate.PROCESS_LOG_MSG_TEMPLATE, "zlm的webrtc注册发送", "发送信息为:", businessMqInfo);
         rabbitMqSender.sendMsgByExchange(dispatcherSignInConf.getMqExchange(), mqGetQueue, UuidUtil.toUuid(),businessMqInfo,true);
 
     }
