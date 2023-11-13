e5182dfce87f3e959ed29e46b6c70185eba0f334
chenjialing <595168663@qq.com>
2023-11-13 13:58:31 +0800

    gb28181--放开告警的mq推送

--- rundo-gb28181-server/src/main/java/com/runjian/gb28181/session/DeviceAlarmCatch.java
+++ rundo-gb28181-server/src/main/java/com/runjian/gb28181/session/DeviceAlarmCatch.java
@@ -156,7 +156,7 @@ public class DeviceAlarmCatch {
             mqInfo.setMsg(BusinessErrorEnums.SUCCESS.getErrMsg());
             String mqGetQueue = gatewaySignInConf.getMqSetQueue();
             log.info(LogTemplate.PROCESS_LOG_MSG_TEMPLATE, "业务场景处理", "告警消息-mq信令发送处理", mqInfo);
-//            rabbitMqSender.sendMsgByExchange(gatewaySignInConf.getMqExchange(), mqGetQueue, UuidUtil.toUuid(), mqInfo, true);
+            rabbitMqSender.sendMsgByExchange(gatewaySignInConf.getMqExchange(), mqGetQueue, UuidUtil.toUuid(), mqInfo, true);
         }
 
     }
